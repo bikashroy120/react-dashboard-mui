627bdbd6c99faaed7a14158eb04366d597aa7a7a
thinkwithabs <twithabs@gmail.com>
2023-01-05 15:32:54 +0600

    first commit

---
+++ src/sences/dashborad/index.jsx
@@ -0,0 +1,15 @@
+import { Box } from '@mui/material'
+import React from 'react'
+import Header from '../../component/Header'
+
+const Dashboard = () => {
+  return (
+    <Box>
+        <Box>
+            <Header title={"DSAHBOARD"} subtitle="Wellcome to our dashboard"/>
+        </Box>
+    </Box>
+  )
+}
+
+export default Dashboard
